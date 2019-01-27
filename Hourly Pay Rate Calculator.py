91507ccdeebc96080b3af82fd233ee3a924fdca8
James Dumitru <36217171+speed785@users.noreply.github.com>
2019-01-26 22:18:27 -0600

    Updated from under hourly pay rate
     added ability to display name, (line 4 still shows error though), added weekly, bi-weekly, monthly, and yearlysimple calculation as well as to show display the info.

--- Hourly Pay Rate Calculator.py
+++ Hourly Pay Rate Calculator.py
@@ -1,9 +1,26 @@
 #Coded by : James Dumitru
 
 # input #
-Name=int(input("Please Enter You're First and Last Name: "))
-Hours=int(input("Please Enter Hours Worked: "))
+Name=input("Please Enter You're First and Last Name: ")
+Hours=int(input("Please Enter You're Hours Worked For This Week: "))
 
 Hourlypayrate=12.50
 
-# weekly, biweekly, monthly, yearly #
+# needs to display weekly, bi-weekly, monthly, yearly #
+
+weekly=round(Hours*Hourlypayrate, 3)
+biweekly=round(weekly*2, 3)
+monthly=round(biweekly*2, )
+yearly=round(monthly*12, 3)
+
+# reveal the weekly,bi-weekly,monthly, and yearly pay in that order#
+
+print(Name ,":")
+
+print("You're weekly pay is: ", weekly)
+
+print("You're biweekly pay is: ", biweekly)
+
+print("You're monthly pay is: ", monthly)
+
+print("You're yearly pay is: ", yearly)
