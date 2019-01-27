6d9b08fa7071d6e1220d20059144d29f70fdf303
James Dumitru <36217171+speed785@users.noreply.github.com>
2019-01-26 21:37:19 -0600

    Creating an Hourly Pay Rate Calculator, created input and the hourly pay rate

---
+++ Hourly Pay Rate Calculator.py
@@ -0,0 +1,9 @@
+#Coded by : James Dumitru
+
+# input #
+Name=int(input("Please Enter You're First and Last Name: "))
+Hours=int(input("Please Enter Hours Worked: "))
+
+Hourlypayrate=12.50
+
+# weekly, biweekly, monthly, yearly #
