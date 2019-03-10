fbc9293ff1bb8bb55ec4cb15dd7823bef66ad0d2
James <ioandumitru59@gmail.com>
2019-03-09 23:34:30 -0600

    Updated Guessing Game Python code.
    Added Welcome to User as well as asking for user's Name. Wrote comments as well as changed the whole structure to a double while loop. I as well changed the # of guesses, as have the code display a error message if the user inputs a character, Example a b c d e f g ... It doesn't count as a turn if entered a character. Again. The number will be randomly generated from 1 through 20.

--- Guessing Game-1.py
+++ Guessing Game-1.py
@@ -1,33 +1,43 @@
 # This Project is called GUESSING GAME
+# Coded by James Dumitru
+
 import random
+import time 
 
-guesses Taken=0
-
-print('Hello! Welcome to the Guessing Game. Guess my number!')
-
+guessesTaken = 0 #starts at 0.
+#Enter Name and welcome.
+print('Hello! Welcome to the Guessing Game. Please Enter Your Name!')
+myName = input()
+#Random number between 1 and 20.
 number=random.randint(1,20)
-print('I am thinking of a number between 0 and 20.')
+print('Hi '+ myName +'! I am thinking of a number between 0 and 20. Guess my number!')
+#Character Error Part 1
+while True:
+    try:
+        #10 guesses max, any more and you lose.
+        while guessesTaken <10:
+            print('Take a guess.') 
+            guess=input()
+            guess=int(guess)
+        #For each guess taken add one
+            guessesTaken=guessesTaken + 1
+        #If too low, you get this message.
+            if guess < number:
+                print('Your guess is too low, try again.')
+        #If too high, you get this message.
+            if guess > number:
+                print('Your guess is too high, try again.') 
+        #If you guess the correct number you get good job and how many tries.
+            if guess == number:
+                guessesTaken = str(guessesTaken)
+                print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+        #If you run out of guesses you get this message, with the correct number.
+        if guess != number:
+            number = str(number)
+            print('Nope. The number I was thinking of was ' + number)
+#Character Error Part 2           
+        break        
+    except ValueError:
+         print("Sorry!  That was not a valid number.  Please try again...")
+##### timer attempt #####
 
-while guessesTaken <5:
-    print('Take a guess.') 
-    guess=input()
-    guess=int(guess)
-
-    guessesTaken=guessesTaken + 1
-
-    if guess < number:
-        print('Your guess is too low.')
-
-    if guess > number:
-         print('Your guess is too high.')
-
-    if guess == number:
-         break  
-
-    if guess == number:
-        guessesTaken = str(guessesTaken)
-        print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
-
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
