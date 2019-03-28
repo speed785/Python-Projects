5d875446cd4d54aa51f95ad58e109b80ef6be498
James <ioandumitru59@gmail.com>
2019-03-28 13:18:56 -0500

    attempt at implementing a shape

--- Project 3.py
+++ Project 3.py
@@ -4,14 +4,21 @@ menu['2']="Filled Square."
 menu['3']="Filled Circle."
 menu['4']="Bow Tie Shape."
 menu['5']="Exit."
+
+
 while True: 
   options=menu.keys()
   options.sort()
 for entry in options: 
-        print entry, menu[entry]
+        print (entry, menu[entry])
     selection=raw_input("Please Select:") 
     if selection =='1': 
-        print "Triangle." 
+        print "Triangle."
+        numberOfRows = int(50) 
+for i in range (numberOfRows):
+for j in range(i + 1):
+        print("*", end="")
+    print()
     elif selection == '2': 
         print "Square"
     elif selection == '3':
